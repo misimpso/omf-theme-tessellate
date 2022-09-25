2e81f1188ce144adedb91c9e1612c79532826804
misimpso <mikeysimpson4@gmail.com>
2022-09-25 12:44:04 -0700

    Fix typo in fish_prompt filename

---
+++ functions/fish_prompt.fish
@@ -0,0 +1,26 @@
+# ------------------------------------
+# Colors
+
+# https://github.com/misimpso/misimpso.github.io/blob/master/LyricChart/scss/main.scss#L101
+# set -l white  DCDCCC
+# set -l grey   2a2a2e
+# set -l red    e81f1f
+# set -l orange e8941e
+# set -l yellow E3CEAB
+# set -l green  51c742
+# set -l blue   42acc7
+# set -l black  ffffff
+
+# set -x color_path $grey $white
+# set -x color_path_basename $grey $white --bold
+# set -x color_venv $blue $grey --bold
+
+
+# ------------------------------------
+# Main
+
+# Inspired by bobthefish
+# https://github.com/oh-my-fish/theme-bobthefish/blob/master/functions/fish_prompt.fish#L1138
+function fish_prompt;
+    echo "wow"
+end
